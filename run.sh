04c4458a88655242cf2bccbe5f16a49404b66320
Ruben Suarez <rubensa@gmail.com>
2019-11-11 08:15:27 +0100

    Add audio and video groups to user

--- run.sh
+++ run.sh
@@ -43,6 +43,7 @@ prepare_docker_sound_host_sharing() {
   MOUNTS+=" --mount type=bind,source=${XDG_RUNTIME_DIR}/pulse,target=${XDG_RUNTIME_DIR}/pulse,readonly"
   # https://github.com/TheBiggerGuy/docker-pulseaudio-example/issues/1
   ENV_VARS+=" --env=PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native"
+  RUNNER_GROUPS+=" --group-add audio"
 }
 
 prepare_docker_webcam_host_sharing() {
@@ -53,6 +54,7 @@ prepare_docker_webcam_host_sharing() {
       DEVICES+=" --device $device"
     fi
   done
+  RUNNER_GROUPS+=" --group-add video"
 }
 
 prepare_docker_gpu_host_sharing() {
