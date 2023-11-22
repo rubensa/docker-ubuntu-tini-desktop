deb01f96fbf4a8927ccc0ad3cfd8ae4b6566b209
Ruben Suarez Alvarez <rubensa@gmail.com>
2023-11-22 08:38:21 +0100

    fix: remove unix prefix from DISPLAY env variable

--- run.sh
+++ run.sh
@@ -96,7 +96,7 @@ prepare_docker_ipc_host_sharing() {
 prepare_docker_x11_host_sharing() {
    # X11 Unix-domain socket
   MOUNTS+=" --mount type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix"
-  ENV_VARS+=" --env=DISPLAY=unix${DISPLAY}"
+  ENV_VARS+=" --env=DISPLAY=${DISPLAY}"
   # Credentials in cookies used by xauth for authentication of X sessions
   if [ -f "${XAUTHORITY}" ]; then
     # IMPORTANT! You MUST run this command before starting the container to update the link
